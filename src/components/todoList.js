ce38bfb656693c7c84ff7aeb8fc6fb47cbc23401
nicol <nicolmadero12@gmaicom>
2021-10-06 23:02:21 -0500

    responsive y estilos aplicados

---
+++ src/components/todoList.js
@@ -0,0 +1,59 @@
+import React,{useState} from 'react'
+import Form from './form'
+import Todo from './todo.js';
+
+function TodoList() {
+    const [todos, setTodos]= useState([]);
+
+    const addTodo = todo =>{
+        if (!todo.text || /^\$*$/.test(todo.text)){
+        return;
+
+    }
+    const newTodos=[todo, ...todos]
+
+    setTodos(newTodos)
+    
+   
+}
+
+const removeTodo = id=>{
+    const removeArray =[...todos].filter(todo => todo.id !==id)
+    setTodos(removeArray)
+}
+
+const updateTodo = (todoId, newValue) =>{
+    if (!newValue.text || /^\$*$/.test(newValue.text)){
+        return;
+    }
+    setTodos(prev =>prev.map(item =>(item.id === todoId ? newValue : item))
+    );
+}
+
+
+const completeTodo = id => {
+    let updateTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updateTodos);
+  };
+
+    return (
+        <div className='todo-app'>
+           <h1>que vamos hacer hoy</h1> 
+          <Form onSubmit={addTodo}/>
+         <Todo
+        todos={todos}
+        completeTodo ={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+
+          />
+        </div>
+    )
+}
+
+export default TodoList
