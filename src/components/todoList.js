efdb2c4e0d6e110894c37b09e6e95d16045cbb44
nicol <nicolmadero12@gmaicom>
2021-10-07 21:12:14 -0500

    barra de busqueda

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,43 +1,47 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect, Component } from 'react'
+import { unmountComponentAtNode } from 'react-dom';
 
 import Form from './form'
 import Todo from './todo.js';
 
 function TodoList() {
-    const [todos, setTodos]= useState([]);
-    const [searchNote, setSearchNote] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [searchNote, setSearchNote] = useState("");
+  const [lista,setLista]=useState('')
 
 
-    const addTodo = todo =>{
-        if (!todo.text || /^\$*$/.test(todo.text)){
-        return;
+  const addTodo = todo => {
+    
+    // console.log(/^\$*$/.test(todo.text))
+    if (!todo.text || /^\$*$/.test(todo.text)) {
+     
+      return;
 
     }
-    const newTodos=[todo, ...todos]
+    const newTodos = [todo, ...todos]
 
     setTodos(newTodos)
-    
-   
-}
-const inputChange = (e) => setSearchNote(e.target.value);
+    console.log(newTodos)
 
 
+  }
 
-const removeTodo = id=>{
-    const removeArray =[...todos].filter(todo => todo.id !==id)
+
+  const removeTodo = id => {
+    const removeArray = [...todos].filter(todo => todo.id !== id)
     setTodos(removeArray)
-}
+  }
 
-const updateTodo = (todoId, newValue) =>{
-    if (!newValue.text || /^\$*$/.test(newValue.text)){
-        return;
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\$*$/.test(newValue.text)) {
+      return;
     }
-    setTodos(prev =>prev.map(item =>(item.id === todoId ? newValue : item))
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item))
     );
-}
+  }
 
 
-const completeTodo = id => {
+  const completeTodo = id => {
     let updateTodos = todos.map(todo => {
       if (todo.id === id) {
         todo.isComplete = !todo.isComplete;
@@ -46,58 +50,78 @@ const completeTodo = id => {
     });
     setTodos(updateTodos);
   };
-  const filterNote = async(objNote, searchNote) => {
-   
-     const notaFiltradaByTitle = await objNote.filter(list=>list.text.toLowerCase().includes(searchNote.toLowerCase()))
-       return notaFiltradaByTitle
-//       // (nota => nota.value.toLowerCase().includes(searchNote.toLowerCase()))
-      // setTodos(notaFiltradaByTitle)
-//       // console.log(nota)
   
-}
-filterNote(todos,searchNote)
 
-const filter=()=>{
-  if(searchNote ===""){
-   console.log(todos);
-  }else{
-    filterNote(todos ,searchNote)
+const onChange= async e =>{
+  e.persist();
+  await setSearchNote(e.target.value)
+  console.log(searchNote)
+}
+const filterNote =( ) => {
+  const notaFiltradaByTitle = todos.filter(list =>{
+      if (list.text.toString().includes(searchNote)){
+       return list
+      }
+    })
+    setSearchNote({lista:notaFiltradaByTitle})
   }
 
-}
+
+  useEffect(()=>{
+    setSearchNote({lista:todos})
+  },[]);
+
+    
+  
+    
+    
+       
+  
+  // } 
+  // filterNote( addTodo(todos), searchNote)
 
 
-useEffect(() => {
- filter();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-}, []);
+
+  return (
+    <div className='todo-app'>
+      
+            
+      <div className="barraBusqueda">
+            <input
+              type="text"
+              placeholder="Buscar"
+              className="textField"
+              name="busqueda"
+              value={searchNote}
+              onChange={onChange}
+              
+              
+            />
+              <button type="button" onClick={filterNote}><i className="fas fa-search"></i></button>
+           
+          </div>
+      {/* <div><input type="text" placeholder="Buscar mi nota"
+        value={searchNote} onChange={inputChange} onKeyUp={ filterNote} />
+      </div> */}
+
+      {/* <button type="button" onClick= ><i className="fas fa-search"></i></button> */}
 
 
+      <h1>que vamos hacer hoy</h1>
 
-    return (
-        <div className='todo-app'>
-          <div><input type="text" placeholder="Buscar mi nota"  
-            value={searchNote} onChange={inputChange} onKeyUp={()=>filterNote} />
-            </div>
-          
-          {/* <button type="button" onClick= ><i className="fas fa-search"></i></button> */}
+      <Form onSubmit={addTodo} />
 
-
-           <h1>que vamos hacer hoy</h1>
-          
-          <Form onSubmit={addTodo}/>
-          
-         <Todo
+      <Todo
         todos={todos}
-        completeTodo ={completeTodo}
+        completeTodo={completeTodo}
         removeTodo={removeTodo}
         updateTodo={updateTodo}
-       
 
-          />
-          
-        </div>
-    )
+
+      />
+
+    </div>
+  )
 }
 
 export default TodoList
